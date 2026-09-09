ae6ae903afc46c9fe8d0c39c95c1a2babaeb809d
cpnandacp <cpnandacp@gmail.com>
2026-09-09 21:04:01 +0530

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "295662564" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3195818207" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
